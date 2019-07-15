ad32391bc88d068ef67da3a3f024c2fa8a028555
vicnatacu <vicki@mvbmb.com>
2019-07-15 14:14:55 +1000

    booking table

---
+++ src/car4rent/db/migrate/20190715040742_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.decimal :cost
+
+      t.timestamps
+    end
+  end
+end
