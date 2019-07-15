7e03c1dc00023b7848043d48055850709b8f809a
vicnatacu <vicki@mvbmb.com>
2019-07-15 14:36:41 +1000

    add devise userowner and userrenter

--- src/car4rent/config/routes.rb
+++ src/car4rent/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :user_renters
+  devise_for :user_owners
   resources :cars
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ src/car4rent/test/models/user_owner_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserOwnerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
