81ac2d30cc4556b1aa4ad73306182a3df7aac013
vicnatacu <vicki@mvbmb.com>
2019-07-15 16:17:44 +1000

    car image

--- src/car4rent/config/routes.rb
+++ src/car4rent/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "bookings#index"
+  
 end
