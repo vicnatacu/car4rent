5a885f22fa27cc294d905cc6ffa6ae6799312dbd
carlmccabe <carl.h.mccabe@gmail.com>
2019-07-15 16:05:01 +1000

    signup/nav started

--- src/car4rent/config/routes.rb
+++ src/car4rent/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :cars
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "bookings#index"
+  root to: "cars#index"
 end
