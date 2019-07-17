b590c5d0a6f45c420d253a4d5de27802d06795c2
lDoughnutl <sam_harvey98@live.com.au>
2019-07-17 13:46:18 +1000

    Final Commit, Fuck this app

--- src/car4rent/config/routes.rb
+++ src/car4rent/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :cars
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cars#index"
 end
